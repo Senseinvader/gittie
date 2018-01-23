98b2172e9041668320ba6542b457db6b4d3c90a4
piotrkaminski95 <pkaminki95@gmail.com>
2018-01-23 10:30:01 +0100

    added joke() to gittie.py

--- gittie.py
+++ gittie.py
@@ -1,6 +1,13 @@
 def introduce():
     print("HELLO, I'm gittie")
 
+
 def add(a,b):
     c = a + b
 return c    
+
+
+def joke():
+    print('Why the chicken crose the road?')
+    input()
+    print('To f*** your mother!')
